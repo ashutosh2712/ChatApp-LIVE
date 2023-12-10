235df1ce8e4f470bc3fac80260a0c74210228356
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-11 03:34:25 +0530

    Display messages and chats between users

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
 
-const Message = () => {
+const Message = ({message}) => {
+
+  const {currentUser} = useContext(AuthContext)
+  const {data} = useContext(ChatContext);
+
+  const ref = useRef()
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({behaviour:"smooth"})
+  }, [message])
   return (
-    <div className='message owner'>
-        <div className="messageInfo">
-            <img src="https://assets.telegraphindia.com/telegraph/2021/Jun/1623340223_loki_review.jpg" alt="" />
-            <span>just now</span>
-        </div>
-        <div className="messageContent">
-            <p>hello</p>
-            <img src="https://assets.telegraphindia.com/telegraph/2021/Jun/1623340223_loki_review.jpg" alt="" />
-        </div>
-    </div>
+      <div  ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+          <div className="messageInfo">
+              <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
+              <span>just now</span>
+          </div>
+          <div className="messageContent">
+              <p>{message.text}</p>
+              {message.img && <img src={message.img} alt="" />}
+          </div>
+  </div>
   )
 }
 
